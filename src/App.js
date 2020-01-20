0e0b6815b603cfa2ddcbafc5b25772993f0b8c1a
nipunadamarakoon <nipunadamarakoon@gmail.com>
2020-01-21 01:22:00 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navBar';
+import Footer from './components/footer';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
+      <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="home">
+           <Navbar/>
+           <Switch>
+               <Route path="/home"  />
+               <Route path="/about"  />
+               <Route path="/shop"  />
+           </Switch>
+
+       </div>
+        <Footer/>
     </div>
+      </Router>
   );
 }
 
 export default App;
+
---
+++ src/components/navBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import Logo from '../images/logo.svg';
+
+
+const Navbar = ()=>{
+
+    return(<nav className='nav-bar' >
+           <div>
+               <div className='logo'><img src={Logo}/></div>
+               <ul className="nav-links">
+
+                   <Link className="link-font" to="#">
+                       <li>
+                           <h3>Featurers</h3>
+                       </li>
+                   </Link>
+
+                   <Link className="link-font" to="#">
+                       <li><h3>Pricing</h3></li>
+                   </Link>
+
+                   <Link className="link-font" to="#">
+                       <li><h3>Resources</h3></li>
+                   </Link>
+
+
+               </ul>
+               <div className='nav-mid'></div>
+               <div className="nav-buttons">
+                   <Link className="link-font" to="#">
+                       <h3>Login</h3>
+                   </Link>
+                   <Link  to="#">
+                       <button>Sign up</button>
+                   </Link>
+               </div>
+
+           </div>
+        </nav>)
+}
+
+export default Navbar;
