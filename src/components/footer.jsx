ca64951aa4d88d7259239c91f50f38867b335a6c
nipunadamarakoon <nipunadamarakoon@gmail.com>
2020-01-22 22:19:19 +0530

    Footer center content fixed

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,8 +6,43 @@ const Footer = ()=>{
 
     return(<div className="footer">
 
+            <div className="center">
+                <Features/>
+                <Resources/>
+                <Company/>
+            </div>
 
     </div>)
 }
 
 export default Footer;
+
+const Features =()=>{
+    return(<div className="footer-content1">
+        <h3>Features</h3>
+        <a>Link Shorting</a>
+        <a>Branded Links</a>
+        <a>Analytics</a>
+    </div>)
+
+}
+const Resources =()=>{
+    return(<div className="footer-content1">
+        <h3>Resources</h3>
+        <a>Blog</a>
+        <a>Developers</a>
+        <a>Support</a>
+    </div>)
+}
+const Company =()=>{
+    return(<div className="footer-content1">
+        <h3>Company</h3>
+
+        <a>About</a>
+        <a>Our team</a>
+        <a>Careers</a>
+        <a>Contact</a>
+        </div>)
+
+}
+
